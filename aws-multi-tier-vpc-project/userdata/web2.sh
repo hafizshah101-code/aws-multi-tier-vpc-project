e1177ceb0c20be2c5201586238ba5348231b66df
hafizshah101-code <hafiz.shah101@gmail.com>
2026-07-01 00:14:56 +0800

    Delete aws-multi-tier-vpc-project directory

--- aws-multi-tier-vpc-project/userdata/web2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-
-echo "<h1>Web Server 2</h1>" > /var/www/html/index.html
-echo "<p>Hosted on AWS EC2</p>" >> /var/www/html/index.html
